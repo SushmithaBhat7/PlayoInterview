744e3f284340113dbb36e6f54b1c700dfeaa378b
Sushmitha Bhat <sushmitha.bhat7@gmail.com>
2021-11-16 18:58:33 +0530

    Created targets, and added 1 Unit and UI test case .

---
+++ TestPlayoUITests/MainTableViewTests.swift
@@ -0,0 +1,40 @@
+//
+//  MainTableViewTests.swift
+//  TestPlayoUITests
+//
+//  Created by Sushmitha Bhat on 16/11/21.
+//
+
+import XCTest
+
+class MainTableViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    func testMainTableViewImage() {
+        let app = XCUIApplication()
+        let screenTitle = app.staticTexts["DetailID"]
+        XCTAssertNotNil(screenTitle.images)
+    }
+    
+
+}
